d00f4ed7b5a66ede153b281c7ccfb0b65f5583bb
Jack Schiavo <jshavs1@gmail.com>
2019-04-13 15:54:16 -0400

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Asteroids.io
+Asteroids inspired multiplayer game utilizing Socket.IO
+
+Clone the project and follow the steps below to setup
+## Setting up the server
+Make sure you have you [node](https://nodejs.org/en/download/) and [npm](https://www.npmjs.com/get-npm) installed on your machine.
+1. Navigate to `Asteroids.io/server/` in the terminal
+2. Run `npm install` to get dependencies
+### Starting the server on localhost
+- Run `npm start` to start the server
+- Or run `nodemon server.js` if you want the server to automatically restart whenever changes are made to the server code
+
+## Setting up the app
+Make sure you have [Carthage](https://github.com/Carthage/Carthage) installed on your machine
+1. Navigate to `Asteroids.io/app/Asteroids.io/`
+2. Run `carthage bootstrap` to install frameworks
+### Running the app with localhost
+Switch the target scheme in Xcode to **Asteroids.io-Dev** and run
+### Running the app with live server
+Switch the target scheme in Xcode to **Asteroids.io**
--- app/Asteroids.io/Asteroids.io/SocketIOManager.swift
+++ app/Asteroids.io/Asteroids.io/SocketIOManager.swift
@@ -32,11 +32,26 @@ class SocketIOManager {
     func configureSocket() {
         print("Configuring Socket")
         
+        // This callback will be executed when the client's socket initially connects to
+        // the server.
         socket.on(clientEvent: .connect) { (data, ack) in
             print("Socket connected to host")
         }
+        
+        // Example of how socket receives data from the server
+        
+        // When the client initially connects to the server, the server automatically sends
+        // a "Welcome!" string through the socket on the "messege" channel. This callback
+        // is executed once the data arrives on the "messege" channel.
         socket.on("messege") { (data, ack) in
+            
+            // The data varibale is an array of JSON objects which only contains 1 element.
+            // Grab the JSON object at 0 index, convert it to a Dictionary and print the value
+            // at the "messege" key.
+            
             var messege = data[0] as! [String : String]
+            
+            // "Welcome!" printed to log
             print(messege["messege"] ?? "error reading messege")
         }
         
