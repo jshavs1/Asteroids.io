55d22f0dea5c6216c31025886009c3311a9c1f7a
Caroline Greer <cgreer@terpmail.umd.edu>
2019-05-04 13:50:02 -0400

    Rotates sprite for the Spaceship with the joystick’s motion

--- app/Asteroids.io/Asteroids.io/Player/Player.swift
+++ app/Asteroids.io/Asteroids.io/Player/Player.swift
@@ -15,6 +15,7 @@ class Player: NetworkObject {
     var ship: Spaceship!
     let speed: CGFloat = 1000
     
+    let DegreesToRadians =  CFloat(Double.pi/180)
     
     override init(owner: String, id: String, transform: NetworkTransform) {
         super.init(owner: owner, id: id, transform: transform)
@@ -32,6 +33,14 @@ class Player: NetworkObject {
     }
     
     override func transformWillChange(newTransform: NetworkTransform) {
+        
+        let myX = ship.position.x - newTransform.position.x
+        let myY = ship.position.y - newTransform.position.y
+        
+        let angle = atan2(myY, myX)
+        
+        ship.zRotation = angle + CGFloat(90 * DegreesToRadians)
+        
         let translate = SKAction.move(to: newTransform.position, duration: deltaTime)
         ship.run(translate)
     }
